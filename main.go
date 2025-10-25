45cb4c38ce623bc51ccd10a4f321c3a64672abf4
Mikhail Belousov <tiger.mananticot@yandex.ru>
2025-10-25 21:54:11 +0300

    added user input

--- main.go
+++ main.go
@@ -7,9 +7,15 @@ import (
 
 func main() {
 	const multiplier = 2
-	userHeight := 1.77
-	userWeight := 65.0
+	var userHeight float64
+	var userWeight float64
+	fmt.Print("Введите свой рост в метрах: ")
+	fmt.Scan(&userHeight)
+	fmt.Print("Введите свой вес: ")
+	fmt.Scan(&userWeight)
+
 	IMT := userWeight / math.Pow(userHeight, multiplier)
 
+	fmt.Print("Ваш индекс массы тела: ")
 	fmt.Print(IMT)
 }
