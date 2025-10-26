949bf8d68df15190d9a26f26c2de66af7c5651c4
Mikhail Belousov <tiger.mananticot@yandex.ru>
2025-10-26 16:14:19 +0300

    refactored a bit

--- main.go
+++ main.go
@@ -6,16 +6,25 @@ import (
 )
 
 func main() {
+	IMT := calculateIMT(getUserInput())
+	outputIMTResult(IMT)
+}
+
+func calculateIMT(userHeight float64, userWeight float64) float64 {
 	const multiplier = 2
+	return userWeight / math.Pow(userHeight/100, multiplier)
+}
+
+func outputIMTResult(imt float64) {
+	fmt.Printf("Ваш индекс массы тела: %.0f", imt)
+}
+
+func getUserInput() (float64, float64) {
 	var userHeight float64
 	var userWeight float64
-	fmt.Print("Введите свой рост в метрах: ")
+	fmt.Print("Введите свой рост: ")
 	fmt.Scan(&userHeight)
 	fmt.Print("Введите свой вес: ")
 	fmt.Scan(&userWeight)
-
-	IMT := userWeight / math.Pow(userHeight, multiplier)
-
-	fmt.Print("Ваш индекс массы тела: ")
-	fmt.Print(IMT)
+	return userHeight, userWeight
 }
